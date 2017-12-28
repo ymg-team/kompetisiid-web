ac8127571f502dfbdae3c1825499031f6799cf6a
yussan <yussandeveloper@gmail.com>
2017-12-28 11:35:15 +0700

    update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
-rm -rf dist-client -rf dist-server && unzip dist-server.zip && unzip dist-client.zip && pm2 restart ki
+echo "update code from gitlab..."
+git pull origin master
+
+echo "rebuild app..."
+rm -rf dist-client -rf dist-server && unzip dist-server.zip && unzip dist-client.zip
+
+echo "restart pm2..."
+pm2 restart ki
+
+echo "deploy finished..."
