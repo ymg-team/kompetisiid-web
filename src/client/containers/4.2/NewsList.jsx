4ddb0d762d0aa33f3495e3b604e761863173d352
yussan <yussandeveloper@gmail.com>
2018-02-15 21:49:51 +0700

    remove unused assets

--- src/client/containers/4.2/NewsList.jsx
+++ src/client/containers/4.2/NewsList.jsx
@@ -4,7 +4,7 @@ import Subheader from '../../components/4.2/Subheader'
 import Newsbox from '../../components/4.2/boxs/NewsBox'
 
 import { fetchBerita, fetchBeritaMore } from '../../../store/berita/actions'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { style, duration } from '../../components/Transtition'
 import Transition from 'react-transition-group/Transition'
 
