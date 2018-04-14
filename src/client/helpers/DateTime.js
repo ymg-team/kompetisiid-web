e83df231e829119a79531a6de8f58f03fb8def17
yussan <yussandeveloper@gmail.com>
2018-04-14 08:36:15 +0700

    refactor(remove deps): remove moment

--- src/client/helpers/DateTime.js
+++ src/client/helpers/DateTime.js
@@ -1,64 +1,59 @@
 /**
  * Created by yussan on 28/02/17.
  */
-import Moment from 'moment'
 
-Moment.locale('id', {
-    month: 'januari_februari_maret_april_mei_juni_juli_agustus_september_oktober_november_desember'.split('_'),
-    monthsShort: 'jan_feb_mar_apr_mei_jun_jul_agu_sep_okt_nov_des'.split('_'),
-    weekdays: 'minggu_senin_selasa_rabu_kamis_jum\'at_sabtu'.split('_'),
-    weekdaysShort: 'min_sen_sel_rab_kam_jum_sab'.split('_'),
-    weekdaysMin: 'mi_se_sl_ra_ka_ju_sa'.split('_'),
-    longDateFormat: {
-        LT: 'HH:mm',
-        L: 'DD/MM/YYYY',
-        LL: 'D MMMM YYYY',
-        LLL: 'D MMMM YYYY LT',
-        LLLL: 'dddd D MMMM YYYY LT'
-    },
-    calendar: {
-        sameDay: '[hari ini pada] LT',
-        nextDay: '[besok pada] LT',
-        nextWeek: 'dddd [pada] LT',
-        lastDay: '[kemarin pada] LT',
-        lastWeek: 'dddd [latest pada] LT',
-        sameElse: 'L'
-    },
-    relativeTime: {
-        future: '%s lagi',
-        past: '%s lalu',
-        s: 'baru saja',
-        m: 'semenit',
-        mm: '%d mnt',
-        h: 'sejam',
-        hh: '%d jam',
-        d: 'sehari',
-        dd: '%d hari',
-        M: 'sebulan',
-        MM: '%d bln',
-        y: 'setahun',
-        yy: '%d thn'
-    },
-    // ordinal: function (number) {
-    //     return number + (number === 1 ? 'er' : 'ème');
-    // },
-    week: {
-        dow: 0, // Monday is the first day of the week.
-        doy: 4 // The week that contains Jan 4th is the first week of the year.
-    }
-})
+const Bulan = [
+  ['jan', 'januari'],
+  ['feb', 'februari'],
+  ['mar', 'maret'],
+  ['apr', 'april'],
+  ['mei', 'mei'],
+  ['jun', 'juni'],
+  ['jul', 'juli'],
+  ['agu', 'agustus'],
+  ['sep', 'september'],
+  ['okt', 'oktober'],
+  ['nov', 'november'],
+  ['des', 'desember'],
+]
 
-Moment.locale('id')
+const timeSegments = [
+  3.154e10,
+  2.628e9,
+  6.048e8,
+  8.64e7,
+  3.6e6,
+  60000,
+  -Infinity
+]
+
+const makeTimeString = (unit, singularString) => (timeSegment, time) =>
+time >= 2 * timeSegment
+  ? `${Math.floor(time / timeSegment)} ${unit} lalu`
+  : singularString
+
+const timeFunctions = [
+  makeTimeString('tahun', 'setahun lalu'),
+  makeTimeString('bulan', 'sebulan lalu'),
+  makeTimeString('minggu', 'seminggu lalu'),
+  makeTimeString('hari', 'sehari lalu'),
+  makeTimeString('jam', 'sejam lalu'),
+  makeTimeString('menit', 'semenit lalu'),
+  () => 'barusaja'
+]
 
 export function epochToRelativeTime(epochtime)
 {
-  return Moment.unix(epochtime).fromNow()
+  const timeDifference = Date.now() - (epochtime)
+  const index = timeSegments.findIndex(time => timeDifference >= time)
+  const timeAgo = timeFunctions[index](timeSegments[index], timeDifference)
+  return timeAgo
 }
 
 export function datetimeToRelativeTime(datetime)
 {
     const date = new Date(datetime)
-    return Moment(datetime).fromNow()
+    return epochToRelativeTime(date)
 }
 
 export function strToDateTime(str)
@@ -66,12 +61,8 @@ export function strToDateTime(str)
     return ''
 }
 
-export function strToCal(str)
-{
-    return Moment(str).format('YYYYMMDD') + 'T000000Z'
-}
-
 export function today(format = 'Do MMM YY')
 {
-    return Moment().format(format)
+    const d = new Date() 
+    return `${d.getDate()} ${Bulan[d.getMonth()][0]} ${d.getFullYear()}`
 }
