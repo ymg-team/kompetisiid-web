142fd3f1c7dde5c24b6854be93800e269e7c22e5
yussan <yussandeveloper@gmail.com>
2018-05-26 14:18:47 +0700

    refactor(news list): using new news api

--- src/client/components/boxs/NewsBox.jsx
+++ src/client/components/boxs/NewsBox.jsx
@@ -5,55 +5,59 @@ import Transition from 'react-transition-group/Transition'
 import { duration, style } from '../Transtition'
 import { Link } from 'react-router-dom'
 
-export default class NewsBox extends Component 
-{
-    generateList(n)
-    {
-        return n.map((n, key) => {
-            return <Card key={key} n={n} />
-        })
-    }
+export default class NewsBox extends Component {
+  generateList(n) {
+    return n.map((n, key) => {
+      return <Card key={key} n={n} />
+    })
+  }
 
-    render()
-    {
-        const {status, message, count, data, is_loading} = this.props
-        return(
-            <div id='news-container'>
-                <div className='container'>
-                    <div className='row no-margin'>
-                    {
-                        this.props.subtitle && status === 200 ?
-                            <span style={{display: 'table'}}>
-                                <br />
-                                menampilkan&nbsp;<strong> {data.length}&nbsp;</strong>dari&nbsp;<strong>{count}&nbsp;</strong>kabar
-                                <br />
-                            </span>
-                        : null
-                    }                    
-                    </div>
-                    <div className='row m-10' />
-                    <div className='row'>
-                        <Transition in={data && data.length > 0} timeout={duration}>
-                            {(state) => (
-                                <div style={Object.assign({}, style.fade.default, style.fade[state])}>
-                                {
-                                    status ?
-                                        !data ? 
-                                            <p className='text-muted'>{message}</p>
-                                        : this.generateList(data)
-                                    : null                            
-                                }
-                             </div> 
-                            )}
-                        </Transition>
-                    </div>
-                    { is_loading || !status ? <Loader /> : null }
+  render() {
+    const { status, message, count, data, is_loading } = this.props
+    return (
+      <div id="news-container">
+        <div className="container">
+          <div className="row no-margin">
+            {this.props.subtitle && data ? (
+              <span style={{ display: 'table' }}>
+                <br />
+                menampilkan&nbsp;<strong>
+                  {' '}
+                  {data.length}&nbsp;
+                </strong>dari&nbsp;<strong>{count}&nbsp;</strong>kabar
+                <br />
+              </span>
+            ) : null}
+          </div>
+          <div className="row m-10" />
+          <div className="row">
+            <Transition in={data && data.length > 0} timeout={duration}>
+              {state => (
+                <div
+                  style={Object.assign(
+                    {},
+                    style.fade.default,
+                    style.fade[state]
+                  )}
+                >
+                  {status ? (
+                    !data ? (
+                      <p className="text-muted">{message}</p>
+                    ) : (
+                      this.generateList(data)
+                    )
+                  ) : null}
                 </div>
-            </div>
-        )
-    }
+              )}
+            </Transition>
+          </div>
+          {is_loading || !status ? <Loader /> : null}
+        </div>
+      </div>
+    )
+  }
 }
 
 NewsBox.defaultProps = {
-    subtitle: true
+  subtitle: true
 }
