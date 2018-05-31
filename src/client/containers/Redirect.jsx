f183bfde0a377c6ff8c19ca01da9596a8183b8bd
yussan <yussandeveloper@gmail.com>
2018-05-31 22:42:10 +0700

    solving error query to obj

--- src/client/containers/Redirect.jsx
+++ src/client/containers/Redirect.jsx
@@ -1,9 +1,7 @@
-import React, {Component} from 'react'
-import {queryToObj} from 'string-manager'
+import React, { Component } from 'react'
 import RedirectCom from '../components/Redirect'
 
 export default class Redirect extends Component {
-  
   constructor(props) {
     super(props)
     const query = queryToObj(this.props.location.search.replace('?', ''))
@@ -12,15 +10,29 @@ export default class Redirect extends Component {
     }
   }
 
-  componentDidMount(){
-    if(!this.state.to) this.props.history.push('/')
+  componentDidMount() {
+    if (!this.state.to) this.props.history.push('/')
   }
-  
+
   render() {
-    if(this.state.to) {
+    if (this.state.to) {
       return <RedirectCom url={this.state.to} />
     } else {
       return null
     }
+  }
+}
+
+function queryToObj(a) {
+  try {
+    var b = {}
+    return (
+      a.split('&').map(function(a) {
+        ;(a = a.split(/=(.+)/)), (b[a[0]] = a[1])
+      }),
+      b
+    )
+  } catch (a) {
+    console.error(a)
   }
 }
