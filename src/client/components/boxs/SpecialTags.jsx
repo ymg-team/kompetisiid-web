14d2dbfaa64ce1eb4ca86cd13fcc52f0dba5a390
yussan <yussandeveloper@gmail.com>
2018-07-23 22:01:54 +0700

    feat(special tags): added special tags in browse page
    Closes #117

---
+++ src/client/components/boxs/SpecialTags.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Styled from "styled-components"
+
+const SpecialTagsStyled = Styled.div`
+  .specialtags-image {
+    float: left;
+    margin-right: 20px;
+    max-width: 100%;
+  }
+  border-bottom: 1px solid lightgray;
+  padding: 25px 0;
+  margin-bottom: 25px;
+`
+
+const SpecialTags = props => {
+  return (
+    <SpecialTagsStyled className="container">
+      <div className="col-md-12">
+        <img className="specialtags-image" src={props.image} />
+        <span className="specialtags-description">
+          <h1>{props.name}</h1>
+          <p>{props.description} <a href={props.link} target="_blank">Selengkapnya</a></p>
+        </span>
+      </div>
+    </SpecialTagsStyled>
+  )
+}
+
+export default SpecialTags
