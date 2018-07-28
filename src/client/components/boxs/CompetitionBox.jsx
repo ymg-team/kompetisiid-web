b1d3e6db96f3ad71f5d3cb64457dfbb5a54d4d0d
yussan <yussandeveloper@gmail.com>
2018-07-28 09:58:33 +0700

    ads test

--- src/client/components/boxs/CompetitionBox.jsx
+++ src/client/components/boxs/CompetitionBox.jsx
@@ -16,7 +16,7 @@ function generateList(size, n) {
             key={`ads_key`}
             adClient="ca-pub-4468477322781117"
             adSlot={5218613800}
-            adTest={true}
+            // adTest={true}
           />
         </div>,
         <Card size={size} key={key} n={n} />
