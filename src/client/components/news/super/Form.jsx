2348e761b75a563a6474668edaf139fc0f50d1c1
yussan <yussandeveloper@gmail.com>
2019-03-23 22:50:46 +0700

    fix(news form): added client side

---
+++ src/client/components/news/super/Form.jsx
@@ -0,0 +1,119 @@
+import React from "react"
+import SubHeader from "../../headers/SubHeader"
+import TitleLevel2Box from "../../boxs/TitleLevel2"
+import InputText from "../../form/InputText"
+import InputFile from "../../form/InputFile"
+import Editor from "../../form/Editor"
+import InputTags from "../../form/InputTags"
+import BtnSubmit from "../../form/Submit"
+import Spacer from "../../boxs/Spacer"
+
+class FormNews extends React.Component {
+
+  static defaultProps = {
+    response: {}
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount = () => {
+    const {newsData, newsId} = this.props
+    if(newsData && newsId) {
+      let nextstate = {
+        title: newsData.title,
+        content: newsData.content,
+        tags: newsData.tag ? newsData.tag.split(",") : []
+      }
+
+      this.setState(nextstate)
+    }
+  }
+
+  submitHandler = () => {
+    let formdata = {
+      title: this.state.title,
+      content: this.state.content,
+      tags: this.state.tags ? this.state.tags.toString() : "",
+    }
+
+    if(this.state.image) formdata.image = this.state.image
+
+    console.log("submit handler...", formdata)
+  }
+
+  render = () => {
+    const {response} = this.props 
+    return (
+      <React.Fragment>
+        <SubHeader
+          customStyle={{ marginTop: 20 }}
+          customClass="row"
+          customClassContent="row"
+          title={this.props.title}
+        />
+        <form
+          className="form-ki no-padding col-md-8"
+          action="javascript:;"
+          method="post"
+        >
+          <TitleLevel2Box
+            title="Data Berita"
+            text="Berita ini adalah asli dan apa adanya."
+          />
+
+          <InputText
+            label="Judul Berita"
+            name="title"
+            type="text"
+            id="input-title"
+            value={this.state.title || ""}
+            validate={this.state.title_validate || {}}
+            placeholder="Masukan judul berita disini"
+            required={true}
+            setState={(n, cb) => this.setState(n, cb)}
+          />
+
+          <InputFile
+            label="Gambar Berita"
+            name="image"
+            id="input-image"
+            value={this.state.image || ""}
+            validate={this.state.image_validate || {}}
+            required={typeof this.props.newsId === "undefined"}
+            setState={(n, cb) => this.setState(n, cb)}
+          />
+
+          <Editor
+            label="Isi Berita"
+            name="content"
+            required={true}
+            value={this.state.content}
+            setState={(n, cb) => this.setState(n, cb)}
+          />
+
+          <InputTags
+            label="Masukan Tag"
+            name="tags"
+            tags={this.state.tags || []}
+            initialValue={this.state.tags}
+            setState={(n, cb) => this.setState(n, cb)}
+          />
+
+          <Spacer size="large" />
+
+          <BtnSubmit
+            disabled={response.is_loading || response.status === 201 || response.status === 200}
+            text={this.props.title}
+            action={() => this.submitHandler()}
+            setState={(n, cb) => this.setState(n, cb)}
+          />
+        </form>
+      </React.Fragment>
+    )
+  }
+}
+
+export default FormNews
