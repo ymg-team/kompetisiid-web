e1f509012e22eb9470aad34ec1a7fd1d92324263
yussan <yussandeveloper@gmail.com>
2019-04-06 11:49:58 +0700

    feat(news api): added endpoint create and update by id

--- src/server/controllers/news.js
+++ src/server/controllers/news.js
@@ -3,27 +3,54 @@
  * created by yussan 23 Oct 2016 18:29
  */
 
-export function getNews(req, res, next)
-{
-  let {params} = req
-  params.query = req.query
+ /**
+  * function to create news
+  * @param {object} req.body
+  */
+export function createNews(req, res, next) {
   req.reqdata = {
-    version: 'v42',
-    method: 'get',
-    params,
-    url: `/v2/news`,
+    version: "v42",
+    method: "post",
+    params: req.body,
+    url: `/v2/news`
   }
 
   next()
 }
 
-export function getNewsDetail(req, res, next)
-{
+/**
+ * function to update news by id
+ * @param {number} req.params.id
+ * @param {object} req.body
+ */
+export function udpateNewsById(req, res, next) {
   req.reqdata = {
-    version: 'v42',
-    method: 'get',
+    version: "v42",
+    method: "put",
+    params: req.body,
+    url: `/v2/news/${req.params.id}`
+  }
+}
+
+export function getNews(req, res, next) {
+  let { params } = req
+  params.query = req.query
+  req.reqdata = {
+    version: "v42",
+    method: "get",
+    params,
+    url: `/v2/news`
+  }
+
+  next()
+}
+
+export function getNewsDetail(req, res, next) {
+  req.reqdata = {
+    version: "v42",
+    method: "get",
     params: req.params,
-    url: `/v2/news/${req.params.id}`,
+    url: `/v2/news/${req.params.id}`
   }
 
   next()
