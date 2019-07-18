5fb95a56b3d7f663a1f811bb3e87cc787157d7db
yussan <yussandeveloper@gmail.com>
2019-07-18 16:49:51 +0700

    wip stats on super page

--- src/client/containers/_super/index.jsx
+++ src/client/containers/_super/index.jsx
@@ -3,16 +3,34 @@ import { connect } from "react-redux"
 
 // components
 import Helmet from "../../components/Helmet"
+import CountBox from "../../components/boxs/_super/CountBox"
+import Loader from "../../components/boxs/GlobalLoader"
+import HeaderDashboard from "../../components/cards/HeaderDashboard"
 
 class SuperContainer extends Component {
   render() {
+    const { stats } = this.props
+    console.log("stats", stats)
     return (
       <React.Fragment>
-        <Helmet title="Dashboard - Kompetisi Id" />
-        <p>This is container of Super Page</p>
+        <Helmet title="Super - Kompetisi Id" />
+        <div className="row">
+          <div className="col-md-12">
+            <HeaderDashboard
+              title="Kompetisi"
+              text="Seluruh data kompetisi di Kommpetisi Id"
+            />
+          </div>
+        </div>
       </React.Fragment>
     )
   }
 }
 
-export default connect()(SuperContainer)
+const mapStateToProps = state => {
+  return {
+    stats: state.Others.count_super_sidebar || {}
+  }
+}
+
+export default connect(mapStateToProps)(SuperContainer)
