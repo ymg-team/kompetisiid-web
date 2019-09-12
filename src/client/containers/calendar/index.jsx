477bbe5615077781e4a0ffdb533f7e44836dd669
yussan <main.yussan@gmail.com>
2019-09-12 11:38:12 +0700

    added fullcalendar assets

--- src/client/containers/calendar/index.jsx
+++ src/client/containers/calendar/index.jsx
@@ -9,10 +9,6 @@ const CalendarContainerStyled = Styled.div`
 `
 
 class CalendarContainer extends React.Component {
-  state = {
-    renderCalender: false
-  }
-
   dateGenerator() {
     return null
   }
@@ -26,7 +22,7 @@ class CalendarContainer extends React.Component {
       <CalendarContainerStyled id="competition-calendar">
         <Helmet title={title} description={desc} />
         <SubHeader title={title} desc={desc} />
-        {this.state.renderCalender ? <CalendarBox /> : null}
+        <CalendarBox />
       </CalendarContainerStyled>
     )
   }
