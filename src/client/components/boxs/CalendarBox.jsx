184baa99923e8f3772f92600f686aa3fced06d77
yussan <yusuf@sebangsa.com>
2019-09-12 13:20:18 +0700

    changed

--- src/client/components/boxs/CalendarBox.jsx
+++ src/client/components/boxs/CalendarBox.jsx
@@ -1,12 +1,6 @@
 import React from "react" 
-// import { Calendar } from '@fullcalendar/core'
-import dayGridPlugin from '@fullcalendar/daygrid'
 
 class CalendarBox extends React.Component {
-
-  componentDidMount() {
-    this.Calendar = require("@fullcalendar/core")
-  }
 
   render () {
     return null
